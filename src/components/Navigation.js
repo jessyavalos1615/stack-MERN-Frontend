f3156679535316e093db299ec44dea62fd6e907c
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2019-08-01 11:11:24 -0600

    First commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+        <Link className="navbar-brand" to="/">
+  Note's App
+  </Link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <Link className="nav-link" to="/">Notes</Link>
+      </li>
+      <li class="nav-item">
+      <Link className="nav-link" to="/create">Create Note</Link>
+      </li>
+      <li class="nav-item">
+      <Link className="nav-link" to="/user">Create User</Link>
+      </li>
+    </ul>
+  </div>
+        </div>
+        </nav>
+    );
+  }
+}
